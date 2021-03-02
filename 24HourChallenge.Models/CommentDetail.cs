1958372c0725c87a0274cd81c5d09bbf88667217
Your Name <you@example.com>
2021-03-02 12:37:33 -0600

    commentModels

---
+++ 24HourChallenge.Models/CommentDetail.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24HourChallenge.Models
+{
+    public class CommentDetail
+    {
+        [Required]
+        public int CommentId { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Content { get; set; }
+    }
+}
---
+++ 24HourChallenge.Models/CommentEdit.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24HourChallenge.Models
+{
+    public class CommentEdit
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+    }
+}
