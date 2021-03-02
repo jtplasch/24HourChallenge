c2106c87fcbaf57687f78713ba839eaafac6e6f1
Your Name <you@example.com>
2021-03-02 13:49:00 -0600

    master

---
+++ 24HourChallenge.Services/CommentService.cs
@@ -0,0 +1,13 @@
+﻿using _24HourChallenge.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24HourChallenge.Services
+{
+    public class CommentService
+    {
+    }
+}
