49976a16e549b282bf635c5cd18c0b9c2cfeb0d4
Your Name <you@example.com>
2021-03-02 13:28:34 -0600

    replyModels

--- 24HourChallenge.Models/ReplyCreate.cs
+++ 24HourChallenge.Models/ReplyCreate.cs
@@ -4,16 +4,13 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-namespace _24HourChallenge.Data
+
+namespace _24HourChallenge.Models
 {
-    public class Comment
+    public class ReplyCreate
     {
-        [Key]
-        public int CommentId { get; set; }
         [Required]
         public string Content { get; set; }
-        [Required]
-        public Guid OwnerId { get; set; }
-        public virtual List<Comment> Comments { get; set; }
+
     }
 }
